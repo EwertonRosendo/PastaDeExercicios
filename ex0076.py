34b9907c2567e4ecaafdc95c69f9992f46ce774a
Rosendo <ewerton.rosendo15@gmail.com>
2022-01-19 11:55:58 -0300

    ex0077
    primeira tentativa

--- ex0076.py
+++ ex0076.py
@@ -2,8 +2,10 @@ listagem = ("Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, "Estojo", 25.00,
 listagem2 = ("Compasso", 9.99, "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.9)
 listagemgeral = listagem + listagem2
 numitem = 0
+texto = str(f"{listagemgeral[numitem]} custa exatos R$ {listagemgeral[numitem+1]:.2f}")
 print("{}INICIO{}".format("-"*15, "-"*15))
+# dividir o texto em 40 ESPAÇOS
 while numitem != len(listagemgeral):
-    print(f"{listagemgeral[numitem]} custa exatos R$ {listagemgeral[numitem+1]:.2f}")
+    print(f"{listagemgeral[numitem]} R$ {listagemgeral[numitem+1]:.2f}")
     numitem += 2
 print("{}FIM{}".format("-"*15, "-"*15))
---
+++ ex0077.py
@@ -0,0 +1,22 @@
+palavras = ("aprender", "programar", "linguagem", "python", "python", "curso", "gratis", "estudar", "praticar", "trabalhar", "mercado", "programador", "futuro",)
+c = 0
+a = "a"
+e = "e"
+i = "i"
+o = "o"
+u = "u"
+while len(palavras) != c:
+    print("Na palavra {} temos ".format(palavras[c]), end=" ")
+    if a in palavras[c]:
+        print("a", end=" ")
+    if e in palavras[c]:
+        print("e", end=" ")
+    if i in palavras[c]:
+        print("i", end=" ")
+    if o in palavras[c]:
+        print("o", end=" ")
+    if u in palavras[c]:
+        print("u", end=" ")
+    print("\n")
+    c += 1
+
