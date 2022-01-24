4f554259f7259e4d4fead7130cfcd8c2be38e96c
Rosendo <ewerton.rosendo15@gmail.com>
2022-01-24 15:58:36 -0300

    Aula 17
    Indrodução aos conceitos e funções atribuidas as listas no Python

---
+++ aula17a.py
@@ -0,0 +1,12 @@
+'''num = [2, 5, 9, 1]
+num.remove(2)
+print(num)
+
+valores = list()
+# ou
+valores1 = []'''
+valores = [5, 8, 4, 7]
+for c, v in enumerate(valores):
+    print(f"Na posição {c} encontrei {v}!")
+
+# b = a[:]
