69d0a33531bb8920890e1e9bd8fc760fd1ba9ac9
Rosendo <ewerton.rosendo15@gmail.com>
2022-01-24 17:05:08 -0300

    Update ex0078.py
    Descrição dos processos adicionada

--- ex0078.py
+++ ex0078.py
@@ -1,13 +1,19 @@
+# Criando uma lista vazia
 valores = list()
+# Inserindo os cinco valores da lista
 for c in range(0, 5):
     valores.append(int(input("Informe um numero para a posição {}: ".format(c))))
-    maior = max(valores)
+# Analisando o maior valor
+maior = max(valores)
 print(f"O maior valor é {max(valores)} na posição ", end="")
+# Pegando as posições em que se encontram os maiores valores
 for x, maior in enumerate(valores):
     if max(valores) == maior:
         print(x, end="..")
 print(f"\nO menor valor é {min(valores)} na posição ", end="")
+# Analisando o menor valor
 menor = min(valores)
+# Pegando as posições em que se encontram os menores valores
 for x, menor in enumerate(valores):
     if min(valores) == menor:
         print(x, end="..")
