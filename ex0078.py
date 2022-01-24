12e2b7d4cc492f0d9b0385e2bfb008a00dac4f76
Rosendo <ewerton.rosendo15@gmail.com>
2022-01-24 17:00:24 -0300

    Update ex0078.py

--- ex0078.py
+++ ex0078.py
@@ -2,7 +2,13 @@ valores = list()
 for c in range(0, 5):
     valores.append(int(input("Informe um numero para a posição {}: ".format(c))))
     maior = max(valores)
-print(f"O maior valor é {max(valores)} na posição ")
+print(f"O maior valor é {max(valores)} na posição ", end="")
 for x, maior in enumerate(valores):
     if max(valores) == maior:
-        print(x)
+        print(x, end="..")
+print(f"\nO menor valor é {min(valores)} na posição ", end="")
+menor = min(valores)
+for x, menor in enumerate(valores):
+    if min(valores) == menor:
+        print(x, end="..")
+print("\nFIM")
