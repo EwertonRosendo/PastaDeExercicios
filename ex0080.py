523badf7607ab03ab7d5c2b23fd79f5929bfafff
Rosendo <ewerton.rosendo15@gmail.com>
2022-01-27 23:08:43 -0300

    add exercicio

--- ex0080.py
+++ ex0080.py
@@ -1,13 +1,20 @@
+x = 0
 valores = list()
-zero = 0
-um = 1
-antigo = 0
-for c in range (0, 5):
-    valores.append(int(input("Informe o valor {}: ".format(c))))
-while True:
-    if valores[0] > min(valores):
-        valores[0] = min(valores)
-        valores[len(valores.index(min(valores)))] = valores[0]
-    else:
-        break
+for c in range(0, 5):
+    x = int(input("Informe um numero: "))
+    # Primeiro valor
+    if c == 0:
+        valores.append(x)
+    # Novo maior valor
+    elif x > max(valores):
+        valores.append(x)
+    # Novo menor valor
+    elif x < min(valores):
+        valores.insert(0, x)
+    elif x > valores[0]:
+        valores.insert(1, x)
+    elif x < valores[4]:
+        valores.insert(3, x)
 print(valores)
+print(max(valores))
+print(min(valores))
