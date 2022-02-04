8700600d0aa9e36b9eadea8d150368a2db7b4324
Rosendo <ewerton.rosendo15@gmail.com>
2022-02-04 16:28:43 -0300

    Update ex0087.py

--- ex0087.py
+++ ex0087.py
@@ -5,9 +5,12 @@ linha = 1
 cont = -1
 soma_de_pares = 0
 soma_terceira_coluna = 0
+maior_valor_linha2 = []
 while True:
     numeros.append(int(input("Informe um valor para a posição {}, {}: ".format(linha, coluna))))
     matriz.append(numeros[:])
+    if linha == 2:
+        maior_valor_linha2.append(numeros[:])
     coluna += 1
     numeros.clear()
     print(len(matriz))
@@ -28,3 +31,4 @@ for c in range(0, 9):
         soma_terceira_coluna += matriz[c][0]
 print(f"\nA soma de todos os numeros pares citados na matriz é {soma_de_pares}")
 print(f"A soma de todos os valores da terceira coluna é: {soma_terceira_coluna} ")
+print(f"O maior valor da segunda linha é {max(maior_valor_linha2)}")
