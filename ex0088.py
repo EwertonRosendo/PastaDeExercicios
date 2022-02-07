1a70218d4928ffdd0798a1ea81bca77b16d6b907
Rosendo <ewerton.rosendo15@gmail.com>
2022-02-07 12:57:55 -0300

    Update ex0088.py

--- ex0088.py
+++ ex0088.py
@@ -1,14 +1,24 @@
 from random import randint
+import time
 palpite = []
 lista_de_palpites = []
 c = 0
-vezes = int(input("Quantos palpites você deseja? "))
+print("{}\nJOGA NA MEGA SENA\n{}".format("="*20, "="*20))
+vezes = int(input("Quantos palpites você quer que seja sorteado? "))
+numero_gerado = 0
+print("{} SORTEANDO {} JOGOS {}".format("=-"*10, vezes, "=-"*10))
 while True:
-    palpite.append(randint(0, 60))
+    numero_gerado = randint(0, 60)
+    if numero_gerado not in palpite:
+        palpite.append(numero_gerado)
     if len(palpite) == 6:
         lista_de_palpites.append(palpite[:])
+        palpite.sort()
         palpite.clear()
         c += 1
     if c == vezes:
         break
-print(lista_de_palpites)
+for c in lista_de_palpites:
+    time.sleep(1.5)
+    print(f"SORTEANDO JOGO {lista_de_palpites.index(c)+1}: {c}")
+print("{} BOA SORTE {}".format("=-"*10, "=-"*10))
