c13b16eb9d31e59c0819fbc7e0c1ec9e71e1ca29
Rosendo <ewerton.rosendo15@gmail.com>
2022-02-17 23:32:51 -0300

    Create EX0090.py

---
+++ EX0090.py
@@ -0,0 +1,22 @@
+lista_de_alunos = []
+nome_e_media = dict()
+continuar = "k"
+while True:
+    nome_e_media = {"Nome": str(input("Informe o nome")), "Nota": float(input("Informe a nota: "))}
+    lista_de_alunos.append(nome_e_media.copy())
+    nome_e_media.clear()
+    continuar = str(input("Deseja continuar? [S/N]")).upper().strip()
+    while continuar not in "S N":
+        continuar = str(input("Informe um valor valido: [S/N]")).strip().upper()
+    if continuar in "N":
+        break
+print(lista_de_alunos)
+for n in lista_de_alunos:
+    for k, v in n.items():
+        print(f" {k} igual a {v} e", end="")
+        if k == "Nota" and v >= 6:
+            print(", Aluno aprovado")
+        if k == "Nota" and v < 6:
+            print(", Aluno reprovado")
+
+    print()
