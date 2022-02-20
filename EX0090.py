cbeb9a32512d045cf9daf89a7e1479c9a5378af7
Rosendo <ewerton.rosendo15@gmail.com>
2022-02-19 21:24:41 -0300

    Update EX0090.py

--- EX0090.py
+++ EX0090.py
@@ -2,7 +2,11 @@ lista_de_alunos = []
 nome_e_media = dict()
 continuar = "k"
 while True:
-    nome_e_media = {"Nome": str(input("Informe o nome")), "Nota": float(input("Informe a nota: "))}
+    nome_e_media = {"Nome": str(input("Informe o nome")), "Nota": float(input("Informe a nota: ")), "Situação": ""}
+    if nome_e_media["Nota"] >= 6:
+        nome_e_media["Situação"] = "Aprovado"
+    else:
+        nome_e_media["Situação"] = "Reprovado"
     lista_de_alunos.append(nome_e_media.copy())
     nome_e_media.clear()
     continuar = str(input("Deseja continuar? [S/N]")).upper().strip()
@@ -13,10 +17,6 @@ while True:
 print(lista_de_alunos)
 for n in lista_de_alunos:
     for k, v in n.items():
-        print(f" {k} igual a {v} e", end="")
-        if k == "Nota" and v >= 6:
-            print(", Aluno aprovado")
-        if k == "Nota" and v < 6:
-            print(", Aluno reprovado")
+        print(f" {k} igual a {v} ", end="")
 
     print()
