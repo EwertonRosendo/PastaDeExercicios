9ea0f315ef534af012e913757bc985c385e67c24
Rosendo <ewerton.rosendo15@gmail.com>
2022-02-23 16:59:29 -0300

    feito com sucesso o exercicio 91
    FINALMENTE

--- EX0090.py
+++ EX0090.py
@@ -3,10 +3,10 @@ nome_e_media = dict()
 continuar = "k"
 while True:
     nome_e_media = {"Nome": str(input("Informe o nome")), "Nota": float(input("Informe a nota: ")), "Situação": ""}
-    if nome_e_media["Nota"] >= 6:
+    if nome_e_media["Nota"] >= 7:
         nome_e_media["Situação"] = "Aprovado"
     else:
-        nome_e_media["Situação"] = "Reprovado"
+        nome_e_media["Situação"] = "Recuperação"
     lista_de_alunos.append(nome_e_media.copy())
     nome_e_media.clear()
     continuar = str(input("Deseja continuar? [S/N]")).upper().strip()
@@ -18,5 +18,4 @@ print(lista_de_alunos)
 for n in lista_de_alunos:
     for k, v in n.items():
         print(f" {k} igual a {v} ", end="")
-
-    print()
+        print()
