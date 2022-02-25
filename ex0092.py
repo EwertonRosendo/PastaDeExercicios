147ef95f0bd1e2846fd2443b063f9c3193cd92bd
Rosendo <ewerton.rosendo15@gmail.com>
2022-02-24 22:00:50 -0300

    atualização da pasta de exercicios

--- ex0092.py
+++ ex0092.py
@@ -8,5 +8,5 @@ if pessoa["carteira_tb"] != 0:
     pessoa["ano_de_contratação"] = int(input("Ano de contratação: "))
     pessoa["salario"] = float(input("Salario: "))
     pessoa["ano_de_aposentar"] = pessoa["ano_de_contratação"]+35
-    pessoa["idade_aposentar"] = ((datetime.datetime.now().year - datetime.datetime.now().year+pessoa["idade"]) + (datetime.datetime.now().year - pessoa["ano_de_contratação"]) + 35)
+    pessoa["idade_aposentar"] = (pessoa["ano_de_contratação"] + 35) - (pessoa["ano_de_contratação"]- pessoa["idade"])
 print(pessoa["idade_aposentar"])
