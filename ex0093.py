afb1867e15ff22fae0597a959af1aafa21f8a685
Rosendo <ewerton.rosendo15@gmail.com>
2022-02-24 22:46:14 -0300

    Update ex0093.py

--- ex0093.py
+++ ex0093.py
@@ -4,16 +4,19 @@ jogador = dict()
 jogador["nome"] = str(input("Informe o nome do jogador: ")).strip()
 jogador["n_de_partidas"] = int(input("Informe o numero de partidas jogadas: "))
 jogador["Gols por partida"] = []
+#  Contagem de gols por partida
 for c in range(0, jogador["n_de_partidas"]):
     jogador["Gols por partida"].append(int(input(f"Informe quantos gols na partida {c}: ")))
-jogador["Total de gols"] = sum(jogador["Gols por partida"])
+jogador["Total de gols"] = sum(jogador["Gols por partida"])  # Soma de todos os gols
 print("-="*20)
 print(jogador)
 print("-="*20)
+#  Informando todos os valores obtidos
 for k, v in jogador.items():
     sleep(1)
     print(f"O campo {k} tem o valor {v}")
 print("-="*20)
+# Exibindo os gols de cada partida
 print("O jogador {} jogou {} partidas.".format(jogador["nome"], jogador["n_de_partidas"]))
 for c, g in enumerate(jogador["Gols por partida"]):
     print(f"=> Na partida {c}, fez {g} gols.")
