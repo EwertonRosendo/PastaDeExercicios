4ef7ad7165ce2784c0015d238ef0b5becc83b81a
ewertonroendo <93278917+EwertonRosendo@users.noreply.github.com>
2022-03-11 23:01:09 -0300

    Conhecendo as funções

---
+++ ex0096.py
@@ -0,0 +1,6 @@
+def area():
+    comprimento = float(input("Informe o comprimento: "))
+    largura = float(input("Informe a largura: "))
+    area = comprimento*largura
+    print(f"A area é {area}m^2")
+area()
