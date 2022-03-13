9ae0681880a85daaca23524b204b3c95680bb5e2
ewertonroendo <93278917+EwertonRosendo@users.noreply.github.com>
2022-03-13 17:45:23 -0300

    Update ex0098.py

--- ex0098.py
+++ ex0098.py
@@ -15,7 +15,7 @@ def contar_ate_10():
 contar_ate_10()
 
 
-#Criando a função de contar de 10 a 0
+#  Criando a função de contar de 10 a 0
 
 def contar_ate_0():
     print("~"*30)
@@ -29,3 +29,47 @@ def contar_ate_0():
 
 #  Chamando a função de 10 a 0 de dois em dois
 contar_ate_0()
+
+
+#  Criando função de contador
+def contador(inicio, fim, passo):
+    print("~"*30)
+    print("CONTADOR PERSONALIZADO")
+    
+    #  FIM > inicio    
+    if fim > inicio and passo > 0:
+        for c in range(inicio, fim + 1, passo):
+            print(c, end=" ")
+            sleep(0.2)
+        print()
+
+    # Fim > inicio e passo == 0
+    if fim > inicio and passo == 0:
+        for c in range(inicio, fim+1, 1):
+            print(c, end=" ")
+            sleep(0.2)
+        print()
+
+    #  Inicio > Fim e Passo > 0    
+    if inicio > fim and passo > 0:
+        for c in range(fim, inicio, -1*passo):
+            print(c, end=" ")
+            sleep(0.2)
+        print()
+    print("~"*30)
+
+    #  Inicio > Fim e passo < 0
+    if inicio > fim and passo < 0:
+        for c in range(fim, inicio, passo):    
+            print(c, end=" ")
+            sleep(0.2)
+        print()
+        
+    # Inicio > Fim e passo == 0
+    if inicio > fim and passo == 0:
+        for c in range(fim, inicio, -1):
+            print(c, end=" ")
+            sleep(0.2)
+        print()
+#  Chamando contador personalizado
+contador(inicio=int(input("Inicio: ")), fim=int(input("FIM: ")), passo=int(input("Passo: ")))
