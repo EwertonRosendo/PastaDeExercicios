51e083ce90c372dece89cf7e432f7cd94689dd61
ewertonroendo <93278917+EwertonRosendo@users.noreply.github.com>
2022-03-27 15:45:24 -0300

    aula21(Parte 2)

--- aula21(Parte-2).py
+++ aula21(Parte-2).py
@@ -1,4 +1,4 @@
-def contador(i, f, p):
+'''def contador(i, f, p):
     """"
     -> Faz uma contagem e mostra na tela
     :param i: Inicio da contagem
@@ -11,4 +11,22 @@ def contador(i, f, p):
 
 help(contador)
 
-contador(2, 10, 1)
+contador(2, 10, 1)'''
+
+
+def fatorial(num, rest = 1):
+    '''
+    -> Mostra o resultado do fatorial
+    :param num: Numero desejado para descobrir seu fatorial
+    :param rest: Resultado do fatorial
+    '''
+    for f in range(num, 0, -1):
+        rest *= f
+    return rest
+
+
+r1 = fatorial(int(input("Informe um numero: ")))
+r2 = fatorial(int(input("Informe um numero: ")))
+r3 = fatorial(int(input("Informe um numero: ")))
+
+print(f"Os resultados dos fatoriais foram {r1}, {r2}, {r3}")
