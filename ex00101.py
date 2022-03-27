c5c3dcc2ede280f3b814980046c477112931d76c
ewertonroendo <93278917+EwertonRosendo@users.noreply.github.com>
2022-03-27 16:00:51 -0300

    ex00101

--- ex00101.py
+++ ex00101.py
@@ -1,15 +1,13 @@
-from datetime import date
-
+import datetime
 
 def voto(nascimento):
-    import datetime
-    if (datetime.datetime.today().year - nascimento) == 16 or (nascimento - datetime.datetime.today().year) == 17:
-        print("Você tem voto opcional")
-
-    elif (datetime.datetime.today().year - nascimento < 16):
-        print("Você não tem nascimento suficiente para votar")
-    
-    elif (datetime.datetime.today().year - nascimento > 17):
+    if datetime.datetime.today().year - nascimento > 17:
         print("Você é obrigado a votar")
 
-voto(int(input("Qual ano você nasceu? ")))
+    elif datetime.datetime.today().year - nascimento < 16:
+        print("Você não tem idade suficiente para votar")
+
+    else:
+        return "Você tem direito a voto opcional"
+
+voto(int(input("Seu ano de nascimento? ")))
