8cb48dfdcdd9c280088da2886c4c8dd8273c8d07
ewertonroendo <93278917+EwertonRosendo@users.noreply.github.com>
2022-03-27 16:06:04 -0300

    Update ex00101.py

--- ex00101.py
+++ ex00101.py
@@ -2,10 +2,10 @@ import datetime
 
 def voto(nascimento):
     if datetime.datetime.today().year - nascimento > 17:
-        print("Você é obrigado a votar")
+        return "Você é obrigado a votar"
 
     elif datetime.datetime.today().year - nascimento < 16:
-        print("Você não tem idade suficiente para votar")
+        return "Você não tem idade suficiente para votar"
 
     else:
         return "Você tem direito a voto opcional"
