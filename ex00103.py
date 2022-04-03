29890a1d7af8e4be5e2f84a6ba06e2627bbf62f7
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-04-03 00:50:05 -0300

    Update ex00103.py

--- ex00103.py
+++ ex00103.py
@@ -5,7 +5,10 @@ def ficha(nomeJogador="<DESCONHECIDO>", qtdGols="<NAO INFORMADO>"):
         int(qtdGols)
     else:
         qtdGols = 0
-
+    if nomeJogador.isalpha():
+        str(nomeJogador)
+    else:
+        nomeJogador = "<DESCONHECIDO>"
     print(f"{'~'*15}FICHA{'~'*15}")
     return f"Nome do JOGADOR     - [{nomeJogador}]\nQuantidade de gols  - [{qtdGols}]"
 print(ficha(nomeJogador=str(input("Nome do jogador: ")), qtdGols=str(input("Quantidade de gols: "))))
