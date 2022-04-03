d61cdf14bb999c76411d8314a377a1b0775d9906
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-04-03 00:40:59 -0300

    Update ex00103.py

--- ex00103.py
+++ ex00103.py
@@ -10,6 +10,5 @@ def ficha(nomeJogador="", qtdGols="-1"):
             jogador["gols"] = "<GOL NAO INFORMADO>"
 
     print(f"{'~'*15}FICHA{'~'*15}")
-    print(f"Nome do JOGADOR     - [{jogador['nome']}]")
-    print(f"Quantidade de gols  - [{jogador['gols']}]")
-ficha(nomeJogador=str(input("Nome do jogador: ")), qtdGols=str(input("Quantidade de gols: ")))
+    return f"Nome do JOGADOR     - [{jogador['nome']}]\nQuantidade de gols  - [{jogador['gols']}]"
+print(ficha(nomeJogador=str(input("Nome do jogador: ")), qtdGols=str(input("Quantidade de gols: "))))
