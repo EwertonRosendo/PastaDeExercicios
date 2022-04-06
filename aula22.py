b3567d37cb90be49b8e72308b039f3a83e59e665
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-04-06 11:26:56 -0300

    aula 22

---
+++ aula22.py
@@ -0,0 +1,5 @@
+import uteis
+
+num = int(input("Informe um numero: "))
+print(f"O fatorial de {num} é {uteis.fatorial(num)}")
+print(f"O dobro de {num} é {uteis.dobro(num)}")
---
+++ uteis.py
@@ -0,0 +1,11 @@
+def fatorial(n):
+    f = 1
+    for c in range(n, 0, -1):
+        f *= c
+        print(c)
+    return f
+
+
+def dobro(n):
+    d = n*2
+    return d
