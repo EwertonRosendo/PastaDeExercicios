cc557b47bdc025fee6729bca0b2276e7b90ceea6
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-04-24 16:48:32 -0300

    Update ex115.py

--- ex115.py
+++ ex115.py
@@ -2,17 +2,21 @@ import pacoteAula115
 cadastros = list()
 while True:
     try:
+
         print(f"{'-' * 40}\n{' ' * 13}MENU PRINCIPAL\n{'-' * 40}")
-        print("\033[34m")
+        print(f"\033[34m")
         print("[1] - Ver pessoas cadastradas ")
         print("[2] - Cadastrar uma nova pessoa ")
         print("[3] - Sair do sistema")
         print("\033[m")
+        print(f"{'-' * 40}")
         opcao = int(input("\033[33mSua opção: \033[m"))
 
         if opcao == 1:
+            print(f"{'-' * 40}\n{' ' * 16}OPÇÃO 1\n{'-' * 40}")
             pacoteAula115.mostrarLista(cadastros)
         elif opcao == 2:
+            print(f"{'-' * 40}\n{' ' * 16}OPÇÃO 2\n{'-' * 40}")
             pacoteAula115.cadastrar(cadastros)
         elif opcao == 3:
             break
