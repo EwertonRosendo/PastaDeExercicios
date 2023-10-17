94610ad37055340dc95ad8f53d3cc045832b52e8
Rosendo <93278917+EwertonRosendo@users.noreply.github.com>
2023-10-17 19:36:16 +0000

    atualizando exercicio

--- matrizes_vetores.py
+++ matrizes_vetores.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 usuarios = [
             {   'id':1,
                 'user':'rosendo',
@@ -27,7 +29,34 @@ senhas =  [{'user_id':'1',
                               {'telegram':'sdkjhfds'}]}
             ]
 
+ids = np.array([1, 2, 3, 4, 5, 6])
+ids2 = np.array([1, 2, 3, 4, 5, 6])
+print('soma de arrays:', ids+ids2)
 
-print(usuarios)
-print()
-print(senhas)
+matriz = np.array([[1, 2, 4], [4, 5, 6]])
+cond = matriz > 2
+
+print(matriz[cond])
+
+print(np.mean(matriz[cond]))
+print(type(ids))
+
+for n in ids:
+    print(f'\n {n} + 2 = {n+2}')
+
+
+
+def num(num, vec):
+
+    for n in vec:
+        print(f'\n {n} + {num} = {n+num}')
+
+print('chamando uma função ai')
+num(5, ids)
+
+print('produto de duas matrizes: \n', np.dot(ids, ids2))
+
+print('menor valor de ids', np.min(ids))
+#print(usuarios)
+#print()
+#print(senhas)
