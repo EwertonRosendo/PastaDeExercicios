c1b19c22d3b0c212526afad4158ff033ef65098b
Rosendo <93278917+EwertonRosendo@users.noreply.github.com>
2023-10-17 19:04:53 +0000

    atividade backend sobre matrizes

---
+++ matrizes_vetores.py
@@ -0,0 +1,33 @@
+usuarios = [
+            {   'id':1,
+                'user':'rosendo',
+                'user_password':'37423asds',
+                'passwords':[{'gmail':'489325943'},
+                              {'netflix':'wehjfjlhsd33'},
+                              {'telegram':'kdjh873'}]
+            },
+            {   'id':2,
+                'user':'alexxx',
+                'user_password':'324u23hgsd',
+                'passwords':[{'gmail':'237asuida'},
+                              {'netflix':'sdfh832'},
+                              {'telegram':'sdkjhfds'}]
+            },
+            ]
+
+senhas =  [{'user_id':'1',
+            'passwords':[{'gmail':'489325943'},
+            {'netflix':'wehjfjlhsd33'},
+            {'telegram':'kdjh873'}]
+            },
+
+            {'user_id':'2',
+             'passwords':[{'gmail':'237asuida'},
+                              {'netflix':'sdfh832'},
+                              {'telegram':'sdkjhfds'}]}
+            ]
+
+
+print(usuarios)
+print()
+print(senhas)
