f3c00e4fa8694d6942e822fb35dff3ebcead76a8
Rosendo <93278917+EwertonRosendo@users.noreply.github.com>
2023-10-17 19:36:34 -0300

    Update matrizes_vetores.py

--- matrizes_vetores.py
+++ matrizes_vetores.py
@@ -4,16 +4,10 @@ usuarios = [
             {   'id':1,
                 'user':'rosendo',
                 'user_password':'37423asds',
-                'passwords':[{'gmail':'489325943'},
-                              {'netflix':'wehjfjlhsd33'},
-                              {'telegram':'kdjh873'}]
             },
             {   'id':2,
                 'user':'alexxx',
                 'user_password':'324u23hgsd',
-                'passwords':[{'gmail':'237asuida'},
-                              {'netflix':'sdfh832'},
-                              {'telegram':'sdkjhfds'}]
             },
             ]
 
